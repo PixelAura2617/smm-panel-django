cbaa69a2bbe2f126b054c8d552f2141b98c05d7e
Ravi <pixelaura17@gmail.com>
2026-05-02 13:40:06 +0530

    env smtp tomeout port

--- smm_panel/settings.py
+++ smm_panel/settings.py
@@ -107,7 +107,7 @@ SMM_API_KEY = os.environ.get("SMM_API_KEY")
 # ---------------- EMAIL ----------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
