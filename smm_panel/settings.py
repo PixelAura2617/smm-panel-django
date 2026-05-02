8c11b8006b7d76e5ea31cc030b77fd2d8c628849
Ravi <pixelaura17@gmail.com>
2026-05-02 13:33:07 +0530

    env smtp setup

--- smm_panel/settings.py
+++ smm_panel/settings.py
@@ -106,14 +106,14 @@ SMM_API_KEY = os.environ.get("SMM_API_KEY")
 
 # ---------------- EMAIL ----------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 
 # ---------------- DEFAULT PK ----------------
